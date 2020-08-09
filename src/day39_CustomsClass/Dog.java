8c340d86b0f1d707039f805cb28b91e0546050ff
minara35 <minara35@gmail.com>
2020-08-09 15:12:15 -0400

    add DogObjects.java

--- src/day39_CustomsClass/Dog.java
+++ src/day39_CustomsClass/Dog.java
@@ -1,7 +1,40 @@
 package day39_CustomsClass;
+/*
+instance variables
+name ,breed , size age color
+instance methods
+setDogINfo , getDogInfo, eat, drink, sleep
+
+ */
 
 public class Dog {
-    public static void main(String[] args) {
+String name;
+    String breed;
+    String size;
+    int age;
+    String color;
+    public void setDogInfo(String dogName, String dogBreed, String dogSize, int dogAge, String dogColor){
+
+        name = dogName;
+     breed = dogBreed;
+     size = dogSize;
+     age = dogAge;
+     color = dogColor;
+
+     // sets the info of the dog
+
 
     }
+    public void getDogInfo(){
+        System.out.println("Name: " + name + ", Breed: " + breed +", Size: " + size + ", Color: " + color + ", Age: " + age + " years old");
+    }
+    // prints the dog info
+    public void eat(String food){
+        System.out.println(name + " is eating " + food);
+    }
+
+    public void drink(String drink){
+        System.out.println(name + " is drinking " + drink);
+    }
+
 }
