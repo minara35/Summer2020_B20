dded628c53e2ced5878f3e541ce849d4e189bcef
minara35 <minara35@gmail.com>
2020-08-09 13:13:58 -0400

    add CarMax.java

--- src/day39_CustomsClass/Car.java
+++ src/day39_CustomsClass/Car.java
@@ -21,7 +21,7 @@ public class Car {
                 color = carColor;
                 mileage = carMileage;
                 price = carPrice;
-
+            // sets the info of the car objects
 
 /*
 car1: Toyota
@@ -31,11 +31,11 @@ car2: BMW
         }
         public void getInfo(){
             System.out.println(year + " " + brand + " " + model + ", " + color+ ", " + mileage+ ", $" + price);
-
+            // 2012 Toyota Corolla, Red, 10000, $45000
         }
 
 
-
+    // prints the car info
          public void print(){
         System.out.println(brand + " " + model + " is started");
     }
--- src/day39_CustomsClass/CarMax.java
+++ src/day39_CustomsClass/CarMax.java
@@ -9,13 +9,13 @@ public class CarMax {
         Car car5 = new Car();
 
         car1.setInfo("Lexus", "RX350", 2019,"White", 14000, 45000);
-car2.setInfo("Lexus", "RX450", 2020,"White", 1000, 50000);
-car3.setInfo("Ford","Escape", 2018, "Black", 25000, 20000 );
-car1.getInfo();
-car2.getInfo();
-car3.getInfo();
+        car2.setInfo("Lexus", "RX450", 2020,"White", 1000, 50000);
+        car3.setInfo("Ford","Escape", 2018, "Black", 25000, 20000 );
+        car1.getInfo();
+        car2.getInfo();
+        car3.getInfo();
 
-car3.print();
+        car3.print();
     }
 
 
