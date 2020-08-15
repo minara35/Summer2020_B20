da17c4acf587e3e9fbdd5fca367f4cafdfdf9b2d
minara35 <minara35@gmail.com>
2020-08-15 11:45:58 -0400

    updated day41.java

--- src/day41_Static/Carpet.java
+++ src/day41_Static/Carpet.java
@@ -10,12 +10,7 @@ create a custom class for the Carpet class that should contain the following:
                         getCarpetInfo(): should be able to display all the info of the carpet including the total cost of the carpet as calculated by calcCost()
             total price of carpet= (width*length)*unitprice
             if the carpet is persian  carpet that's came from Turkey, add 200$ to the totalPrice
-            create a class called carpetObjects, and create an array of the carpet that contains 5 carpet objects ( make sure you set the instance variables of those objets)
-            create two ArrayList of carpets:
-                                        persianCarpets
-                                        reugularCarpets
-            write a program to store all the persian Carpets into the list of persianCarpets, and store all regular carpets into the list of reugularCarpets
-                    use for each loop to print out all the persian carpets
+
 
  */
 public class Carpet {
