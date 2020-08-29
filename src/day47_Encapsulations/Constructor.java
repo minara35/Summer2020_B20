ac3f43b43b35748c02e8d87d8836f2e2fbf8b4da
minara35 <minara35@gmail.com>
2020-08-29 16:38:25 -0400

    update day47

--- src/day47_Encapsulations/Constructor.java
+++ src/day47_Encapsulations/Constructor.java
@@ -1,27 +1,35 @@
 package day47_Encapsulations;
 
 public class Constructor {
+    static{
+        System.out.println("static block");
+    }
+
     public Constructor(int a){
 
         System.out.println("int arg");
-
     }
+
     public Constructor(double a){
-        this(10); // int arg
-        System.out.println("double arg"); // double arg
-
-
+        this(10);  //int arg
+        // this("A");
+        System.out.println("double arg");  // double arg
     }
-    public  Constructor(String a){
-        System.out.println("String arg");
+
+    public Constructor(String a){
+        this(5.5); // int, double
+        System.out.println("String arg"); // string
     }
+
     public static void main(String[] args) {
-      //  new Constructor();
 
-        new Constructor(10);
-        new Constructor(10.0);
+        //new Constructor();
+
+        new Constructor("A");
+
 
     }
+
 
 
 
