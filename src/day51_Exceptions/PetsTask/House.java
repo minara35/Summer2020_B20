9fe5a2a3c12b6fe7c67f725238fa83bd90f00f5f
minara35 <minara35@gmail.com>
2020-09-05 14:38:34 -0400

    add day 51

---
+++ src/day51_Exceptions/PetsTask/House.java
@@ -0,0 +1,9 @@
+package day51_Exceptions.PetsTask;
+
+public class House {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Wesky", "Haski", 'M', 2, "white");
+        System.out.println(dog1.name);
+        System.out.println(dog1);
+    }
+}
