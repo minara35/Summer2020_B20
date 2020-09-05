96618f20d9ae7b2078421da8e2aa683fe75872c1
minara35 <minara35@gmail.com>
2020-09-05 11:06:45 -0400

    add day 51

---
+++ src/day51_Exceptions/methodOverloading.java
@@ -0,0 +1,17 @@
+package day51_Exceptions;
+
+public class methodOverloading {
+    public void method(){
+
+    }
+
+
+    private void method1(){
+
+    }
+
+
+    protected long method(long a){
+        return 10;
+    }
+}
---
+++ src/day51_Exceptions/methodOverriding.java
@@ -0,0 +1,20 @@
+package day51_Exceptions;
+
+
+
+
+    class A{
+        public void method(){
+
+        }
+    }
+public class methodOverriding extends A{
+        @Override
+    public void method(){
+
+    }
+
+    public String toString(){
+            return "example";
+    }
+}
