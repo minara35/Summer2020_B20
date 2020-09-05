d89076a2547ae3835ee3a6ec6e5f17f1700f28d1
minara35 <minara35@gmail.com>
2020-09-05 16:48:08 -0400

    add day 51

--- src/Library/Util.java
+++ src/Library/Util.java
@@ -479,6 +479,15 @@ public class Util {
     //sorts Character array in descending order
 
 
+    public static void sleep(double seconds) {
+        try {
+            Thread.sleep((long)(seconds * 1000));
+        } catch (InterruptedException e) {
+
+        }
+    }
+
+
 
 
 
---
+++ src/day51_Exceptions/ExceptionsIntro.java
@@ -0,0 +1,14 @@
+package day51_Exceptions;
+
+public class ExceptionsIntro {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        System.out.println("Test started");// unchecked exceptions (unexpected event)
+       // Thread.sleep(3000); // checked exceptions (unwanted event)
+String str = "cybertek";
+        System.out.println(str.charAt(-1)); // unchecked
+
+        System.out.println("step 2");
+    }
+}
