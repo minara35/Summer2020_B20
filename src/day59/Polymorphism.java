b8d2848fc8baa22e0ce4e95e85e2eefc3e17673e
minara35 <minara35@gmail.com>
2020-10-03 12:44:06 -0400

    add day 60

---
+++ src/day59/Polymorphism.java
@@ -0,0 +1,26 @@
+package day59;
+
+import day55_Abstarctioin.ShapesTask.Circle;
+import day55_Abstarctioin.ShapesTask.Rectangle;
+import day55_Abstarctioin.ShapesTask.Shape;
+
+public class Polymorphism {
+    public static void main(String[] args) {
+
+        Shape shape1 = new Circle(3);
+
+        // System.out.println( shape1.radius );
+
+        System.out.println(((Circle) shape1).radius);
+
+        // shape1.length;
+        //  System.out.println(   ( (Rectangle) shape1 ).length     );
+
+
+        Rectangle r1 = new Rectangle(3, 5);
+
+        Shape shape2 = (Shape) r1; // upcasting
+
+        // Shape shape3 = (Circle)r1;
+    }
+    }
